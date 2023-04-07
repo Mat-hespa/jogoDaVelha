e2ead1778fadf96bc263a87a3b149ddbcc8d95d3
Matheus Hespagnola <matheusah@ciandt.com>
2023-04-07 11:59:34 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,29 @@
+from jogoDaVelha import *
+from minimax import *
+
+jogador = 0
+board = criaBoard()
+ganhador = verificaGanhador(board)
+
+while(not ganhador):
+    printBoard(board)
+    print("******************************")
+    if(jogador == 0):
+        i,j = movimentoIA(board, jogador)
+    else:
+        i,j = movimentoIA(board, jogador)
+        i = getInputValido("Digite a linha: ")
+        j = getInputValido("Digite a coluna: ")
+    
+    if(verificaMovimento(board, i, j)):
+        fazMovimento(board, i, j, jogador)
+        jogador = (jogador + 1)%2
+    else:
+        print("Posição ocupada")
+    
+    ganhador = verificaGanhador(board)
+
+print()
+printBoard(board)
+print()
+print("Ganhador = ", ganhador)
